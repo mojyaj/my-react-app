53ed68e2a9af64b01d47128b6fd15dbf87b75c15
mojyaj <yevaj72@gmail.com>
2026-04-29 22:21:23 -0500

    onChange Event Handler
    onChange used to reflect changes or user input onto the UI for responsive interaction

---
+++ old-files/4-28-2026/src-useState-hook/MyComponent.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+export default function MyComponent() {
+
+    const [name, setName] = useState("Guest");
+    const [age, setAge] = useState(0);
+    const [isEmployed, setIsEmployed] = useState(false);
+
+    const updateName = () => {
+
+        setName("Spongebob");
+    }
+    const incrementAge = () => {
+
+        setAge(age + 1);
+    }
+    const toggleEmployStatus = () => {
+
+        setIsEmployed(!isEmployed);
+    }
+
+    return(
+        <div>
+            <p>Name: {name} </p>
+            <button onClick={updateName}> Set Name </button>
+
+            <p>Age: {age} </p>
+            <button onClick={incrementAge}> Increment Age </button>
+
+            <p>Is employed: {isEmployed ? "Yes" : "No"} </p>
+            <button onClick={toggleEmployStatus}> Employment Status </button>
+        </div>
+    );
+}
